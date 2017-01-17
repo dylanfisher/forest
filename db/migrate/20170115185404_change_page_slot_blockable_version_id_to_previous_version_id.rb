397aa041fb6fe13a3c5e92ece3cb077879687fcd
dylanfisher <hi@dylanfisher.com>
2017-01-17 00:34:39 -0500

    Remove migration

--- db/migrate/20170115185404_change_page_slot_blockable_version_id_to_previous_version_id.rb
+++
@@ -1,8 +0,0 @@
-class ChangePageSlotBlockableVersionIdToPreviousVersionId < ActiveRecord::Migration[5.0]
-  def up
-    remove_index :page_slots, name: :index_page_slots_on_blockable_type_and_blockable_version_id
-    remove_index :page_slots, name: :index_page_slots_on_blockable_version_id_and_blockable_type
-
-    rename_column :page_slots, :blockable_version_id, :blockable_previous_version_id
-  end
-end
