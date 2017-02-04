0f5d5ace3f7922a625681f07b9d427e5c1e5f738
dylanfisher <hi@dylanfisher.com>
2017-02-04 16:06:41 -0500

    Add remote form tag submitter to media items modal

--- app/assets/javascripts/forest/admin/partials/select_tag_form_submitter.js
+++ app/assets/javascripts/forest/admin/partials/select_tag_form_submitter.js
@@ -1,3 +1,4 @@
+// Default selects
 $(document).on('change', '.select-tag--default', function() {
   $(this).closest('.select-tag-form').submit();
 });
@@ -11,27 +12,29 @@ $(document).on('submit', '.select-tag-form', function(e) {
   Turbolinks.visit(url);
 });
 
-// App.pageLoad.push(function() {
-//   var $placeholder;
+// Remote selects
+$(document).on('change', '.select-tag--remote', function() {
+  $(this).closest('.select-tag-remote-form').submit();
+});
 
-//   $('.select-tag--default').each(function() {
-//     var $select = $(this);
-//     var title = $select.attr('data-form-title');
-//     var placeholder = {
-//       id: '',
-//       text: title
-//     };
-//     var allowClear = true;
+$(document).on('submit', '.select-tag-remote-form', function(e) {
+  e.preventDefault();
 
-//     if ( !title ) {
-//       placeholder = false;
-//       allowClear = false;
-//     }
+  var $form = $(this);
+  var $select = $form.find('.select-tag--remote');
+  var $parent = $form.closest( $select.attr('data-remote-parent') );
+  var $target = $parent.find( $select.attr('data-remote-target') );
+  var responseTargetSelector = $select.attr('data-remote-response-target');
+  var $mediaItemChooser = $form.closest('.media-item-chooser');
+  var url = $form.attr('action');
 
-//     $select.select2({
-//       placeholder: placeholder,
-//       allowClear: allowClear,
-//       minimumResultsForSearch: Infinity
-//     });
-//   });
-// });
+  $.get(url, $form.serialize(), function(data) {
+    var content = $(data).find( responseTargetSelector ).html();
+
+    $target.html(content);
+
+    if ( $mediaItemChooser.length ) {
+      App.InfiniteLoader.initialize( $mediaItemChooser.find('[data-infinite-load]'), { $scrollListener: $mediaItemChooser } );
+    }
+  });
+});
