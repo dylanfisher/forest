31cd224da010b216c9a37a4b812586c6babc19b0
dylanfisher <hi@dylanfisher.com>
2017-04-08 17:52:15 -0400

    Proper asset path for favicons

--- lib/forest/engine.rb
+++ lib/forest/engine.rb
@@ -14,6 +14,11 @@ module Forest
         forest/application_admin.css forest/application_public.css
         forest/application_admin.js forest/application_public.js
         jquery2.js
+        forest/favicons/apple-touch-icon.png
+        forest/favicons/favicon-32x32.png
+        forest/favicons/favicon-16x16.png
+        forest/favicons/manifest.json
+        forest/favicons/safari-pinned-tab.svg
       )
     end
   end
