cbc6e330835e0c21ea8058ab5f56038ef1e982a5
dylanfisher <hi@dylanfisher.com>
2017-04-15 23:46:43 -0400

    Subnav helper

---
+++ app/helpers/navigation_helper.rb
@@ -0,0 +1,18 @@
+module NavigationHelper
+
+  def subnav
+    # TODO: cache subnav
+    if parent.present?
+      parent_page = Page.find_by_path(parent)
+      children = parent_page.immediate_children
+      render 'shared/subnav', items: children if children.any?
+    end
+  end
+
+  private
+
+    def parent
+      request.path.split('/').reject(&:blank?)[-2]
+    end
+
+end
