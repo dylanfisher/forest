48f4cc2a6f21c552a21bba4477431888d83f3787
dylanfisher <hi@dylanfisher.com>
2017-07-29 17:17:40 -0400

    Make forest-table rows clickable

---
+++ app/assets/javascripts/forest/admin/partials/forest_tables.js
@@ -0,0 +1,30 @@
+// Forest tables
+
+$(document).on('mouseenter', '.forest-table tbody tr', function() {
+  var $row = $(this);
+
+  $row.addClass('active');
+
+  $(document).one('turbolinks:before-cache.forestTables', function() {
+    $row.removeClass('active');
+  });
+});
+
+$(document).on('mouseleave', '.forest-table tbody tr', function() {
+  var $row = $(this);
+
+  $row.removeClass('active');
+});
+
+$(document).on('click', '.forest-table tbody tr', function(e) {
+  var $row = $(this);
+
+  if ( !$(e.target).closest('a').length ) {
+    var $button = $row.find('a.btn-primary:first');
+    var url = $button.attr('href');
+
+    if ( url ) {
+      Turbolinks.visit(url);
+    }
+  }
+});
