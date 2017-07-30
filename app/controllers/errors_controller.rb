32937805968072d86cfef9d2ebec11c35e650bce
dylanfisher <hi@dylanfisher.com>
2017-07-30 01:56:48 -0400

    Errors controller layout

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,6 @@
-class ErrorsController < ApplicationController
+class ErrorsController < ForestController
+  before_action :set_layout
+
   def not_found
     render status: 404
   end
@@ -6,4 +8,15 @@ class ErrorsController < ApplicationController
   def internal_server_error
     render status: 500
   end
+
+  private
+
+    def set_layout
+      # TODO: figure outhow to determine layout in errors controller
+      if admin?
+        self.class.layout 'admin'
+      else
+        self.class.layout 'public'
+      end
+    end
 end
