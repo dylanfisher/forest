92d8330a80edf9c83efcf5907f5808cdf796966a
dylanfisher <hi@dylanfisher.com>
2017-09-03 21:59:49 -0400

    Add embedded svg helper

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,4 +1,14 @@
 module ImageHelper
+  # Embed an SVG file inline, allowing it to be styled with CSS.
+  def embedded_svg(filename, options={})
+    file = File.read(Rails.root.join('app', 'assets', 'images', 'svg', "#{filename}.svg"))
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+    if options[:class].present?
+      svg['class'] = options[:class]
+    end
+    doc.to_html.html_safe
+  end
 
   def responsive_image(media_item, options = {})
     # TODO: DF 08/04/17 - return a missing image if media item is blank?
@@ -28,5 +38,4 @@ module ImageHelper
     srcset = srcset.map { |src, size| "#{asset_path(src)} #{size}" }.join(', ')
     image_tag source, options.merge(srcset: srcset)
   end
-
 end
