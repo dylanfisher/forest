42f38dda14ea58ae629fb00362a68b0c51efe539
dylanfisher <hi@dylanfisher.com>
2017-11-27 16:11:16 -0500

    Enable nested sortable field sets

--- app/assets/javascripts/forest/admin/partials/sortable_field_sets.js
+++ app/assets/javascripts/forest/admin/partials/sortable_field_sets.js
@@ -1,5 +1,15 @@
 // Sortable fields
 
+// Sortable field sets can be nested multiple times, but need to follow this hierarchy:
+// <div class="sortable-field-set">
+//   <div class="sortable-field">
+//     <div class="sortable-field-set__handle"></div>
+//     <div class="sortable-field-set__position">
+//       <input type="hidden">
+//     </div>
+//   </div>
+// </div>
+
 $(document).on('turbolinks:load app:block-slot-after-insert', function() {
   var $fieldSets = $('.sortable-field-set');
 
@@ -19,20 +29,20 @@ $(document).on('turbolinks:load app:block-slot-after-insert', function() {
       calculatePositions( $fieldSet );
     });
 
-    function calculatePositions($fieldSet) {
-      var $fields = $fieldSet.find('> .sortable-field');
-
-      $fields.each(function(index) {
-        var $field = $(this);
-        var $input = $field.find('.sortable-field-set__position');
-
-        $input.val(index);
-      });
-    }
-
     $(document).one('turbolinks:before-cache', function() {
       $fieldSet.off('sortupdate.sortableFieldSets');
       $fieldSet.sortable('destroy');
     });
   });
+
+  function calculatePositions($fieldSet) {
+    var $fields = $fieldSet.find('> .sortable-field');
+
+    $fields.each(function(index) {
+      var $field = $(this);
+      var $input = $field.find('> .sortable-field-set__position input');
+
+      $input.val(index);
+    });
+  }
 });
