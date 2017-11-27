97cf01548bf50d584879e5d762f5f7b29d27191f
dylanfisher <hi@dylanfisher.com>
2017-11-27 10:22:57 -0500

    Add scheduled status to statusable concern

--- app/models/concerns/statusable.rb
+++ app/models/concerns/statusable.rb
@@ -2,10 +2,12 @@ module Statusable
   extend ActiveSupport::Concern
 
   included do
+    parent_class = self
+
     enum status: {
       published: 1,
       # draft: 2,
-      # scheduled: 3,
+      scheduled: 3,
       # pending: 4,
       hidden: 5
     }
@@ -13,6 +15,13 @@ module Statusable
     validates_presence_of :status
 
     scope :by_status, -> (status) { where(status: status) }
+    scope :published_or_scheduled, -> {
+      where("#{parent_class.model_name.plural}.status = :published OR (#{parent_class.model_name.plural}.status = :scheduled AND COALESCE(#{parent_class.model_name.plural}.scheduled_date, :date_yesterday) <= :date_today)",
+        published: 1,
+        scheduled: 3,
+        date_today: Date.today,
+        date_yesterday: Date.yesterday)
+    }
 
     def self.statusable?
       true
