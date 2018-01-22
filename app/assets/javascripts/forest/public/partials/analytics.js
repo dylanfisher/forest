1f01f4f5bf0b911dd9a848467e9dea2b9e780124
dylanfisher <hi@dylanfisher.com>
2018-01-21 23:26:36 -0500

    Add documentation for configuring Google Analytics

--- app/assets/javascripts/forest/public/partials/analytics.js
+++
@@ -1,8 +0,0 @@
-// Analytics
-
-$(document).on('turbolinks:load', function(e) {
-  // Register Google Analytics pageview
-  if ( typeof gtag === 'function' ) {
-    gtag('config', App.analytics.id, { 'page_path': e.originalEvent.data.url });
-  }
-});
