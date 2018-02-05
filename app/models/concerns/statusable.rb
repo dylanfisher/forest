6c2314003e56e9d30dc0ad345974451d61974140
dylanfisher <hi@dylanfisher.com>
2018-02-04 19:06:57 -0500

    Add published scope to statusable concern

--- app/models/concerns/statusable.rb
+++ app/models/concerns/statusable.rb
@@ -19,6 +19,7 @@ module Statusable
     before_save :check_active_scheduled_date
 
     scope :by_status, -> (status) { where(status: status) }
+    scope :published, -> { by_status(:published) }
     scope :published_or_scheduled, -> {
       where("#{parent_class.model_name.plural}.status = :published OR (#{parent_class.model_name.plural}.status = :scheduled AND COALESCE(#{parent_class.model_name.plural}.scheduled_date, :date_yesterday) <= :date_today)",
         published: 1,
