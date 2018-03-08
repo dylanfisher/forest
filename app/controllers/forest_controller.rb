ceab4cff5d72ee66024272b3182e62f3ee8142ca
dylanfisher <hi@dylanfisher.com>
2018-03-07 21:51:51 -0500

    Skip verify_authenticity_token for sessions#create

--- app/controllers/forest_controller.rb
+++ app/controllers/forest_controller.rb
@@ -2,6 +2,7 @@ class ForestController < ApplicationController
   include Pundit
 
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
 
   layout :layout_by_resource
 
