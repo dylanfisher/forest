d5fb402c24ec152a11d13e3ab06cde88c8cc40e9
dylanfisher <hi@dylanfisher.com>
2018-04-16 13:00:25 -0400

    Add a fixed position update button to edit header

--- app/assets/javascripts/forest/admin/partials/edit_header.js
+++ app/assets/javascripts/forest/admin/partials/edit_header.js
@@ -4,3 +4,19 @@ $(document).on('click', '.update-record-button', function() {
 
   $submitButton.trigger('click');
 });
+
+$(document).on('turbolinks:load', function() {
+  var $fixedButton = $('#fixed-update-record-button');
+
+  if ( $fixedButton.length ) {
+    $(window).on('scroll.editHeader', $.throttle( 500, function() {
+      if ( App.scrollTop > App.windowHeight / 2 ) {
+        $fixedButton.addClass('fixed-update-record-button--active');
+      } else {
+        $fixedButton.removeClass('fixed-update-record-button--active');
+      }
+    }));
+  } else {
+    $(window).off('scroll.editHeader');
+  }
+});
--- lib/forest/version.rb
+++ lib/forest/version.rb
@@ -1,3 +1,3 @@
 module Forest
-  VERSION = '0.33.0'
+  VERSION = '0.33.1'
 end
