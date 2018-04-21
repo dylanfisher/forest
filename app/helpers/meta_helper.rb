2398ddf523952a94b6275df801d865fb14d952e9
dylanfisher <hi@dylanfisher.com>
2018-04-21 13:06:14 -0400

    Add og:image:width and other meta tags to head. Memoize all meta helpers

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -1,86 +1,169 @@
 module MetaHelper
   def meta_page_title
-    title = [
-      page_title,
-      site_title
-    ].reject(&:blank?).join(divider)
-    stripdown(title).squish
+    @_meta_page_title ||= begin
+      title = [
+        page_title,
+        site_title
+      ].reject(&:blank?).join(divider)
+      stripdown(title).squish
+    end
   end
 
   def page_title
-    title = [
-      content_for(:page_title),
-      @page_title,
-      build_page_title_from_record,
-      build_page_title_from_controller
-    ].reject(&:blank?).first
-    stripdown(title).squish
+    @_page_title ||= begin
+      title = [
+        content_for(:page_title),
+        @page_title,
+        build_page_title_from_record,
+        build_page_title_from_controller
+      ].reject(&:blank?).first
+      stripdown(title).squish
+    end
   end
 
   def site_title
-    a = Setting.for('site_title') || default_site_title
-    stripdown(a).squish
+    @_site_title ||= begin
+      title = Setting.for('site_title') || default_site_title
+      stripdown(title).squish
+    end
   end
 
   def page_description
-    description = [
-      content_for(:page_description),
-      @page_description,
-      build_page_description_from_record,
-      site_description
-    ].reject(&:blank?).first.squish
-    stripdown(description).squish
+    @_page_description ||= begin
+      description = [
+        content_for(:page_description),
+        @page_description,
+        build_page_description_from_record,
+        site_description
+      ].reject(&:blank?).first.squish
+      stripdown(description).squish
+    end
   end
 
   def page_keywords
-    [
-      content_for(:page_keywords),
-      Setting.for('page_keywords')
-    ].reject(&:blank?).first.try(:squish)
+    @_page_keywords ||= begin
+      [
+        content_for(:page_keywords),
+        Setting.for('page_keywords')
+      ].reject(&:blank?).first.try(:squish)
+    end
   end
 
   def page_featured_image
-    [
-      content_for(:page_featured_image),
-      build_page_featured_image_from_record,
-      site_featured_image
-    ].reject(&:blank?).first.try(:squish)
+    @_page_featured_image ||= begin
+      image = [
+        content_for(:page_featured_image),
+        @page_featured_image,
+        build_page_featured_image_from_record,
+        site_featured_image
+      ].reject(&:blank?).first
+
+      if image.respond_to?(:attachment)
+        image = image.attachment
+      end
+
+      image
+    end
+  end
+
+  def page_featured_image_url
+    begin
+      @_page_featured_image_url ||= begin
+        if page_featured_image.is_a?(String)
+          url = page_featured_image
+        end
+
+        if page_featured_image.is_a?(Paperclip::Attachment)
+          if page_featured_image.options[:styles].keys.include?(:large)
+            style = :large
+          else
+            style = page_featured_image.options[:styles].keys.last
+          end
+          url = page_featured_image.url(style)
+        end
+
+        url
+      end
+    rescue Exception => e
+    end
+  end
+
+  def page_featured_image_width
+    @_page_featured_image_width ||= begin
+      return unless page_featured_image.is_a?(Paperclip::Attachment)
+      page_featured_image.instance.try(:dimensions).try(:[], :width) ||
+        page_featured_image.instance.try(:attachment_width)
+    end
+  end
+
+  def page_featured_image_height
+    @_page_featured_image_height ||= begin
+      return unless page_featured_image.is_a?(Paperclip::Attachment)
+      page_featured_image.instance.try(:dimensions).try(:[], :height) ||
+        page_featured_image.instance.try(:attachment_height)
+    end
+  end
+
+  def page_featured_image_type
+    @_page_featured_image_type ||= begin
+      return unless page_featured_image.is_a?(Paperclip::Attachment)
+      page_featured_image.try(:attachment_content_type)
+    end
+  end
+
+  def page_featured_image_alt
+    @_page_featured_image_alt ||= begin
+      return unless page_featured_image.is_a?(Paperclip::Attachment)
+      page_featured_image.try(:alternative_text)
+    end
   end
 
   def site_description
-    (Setting.for('description') || default_site_description).try(:squish)
+    @_site_description ||= begin
+      (Setting.for('description') || default_site_description).try(:squish)
+    end
   end
 
   def site_featured_image
-    Setting.for('featured_image')
+    @_site_featured_image ||= begin
+      Setting.for('featured_image')
+    end
   end
 
   def meta_see_also
-    [
-      content_for(:meta_see_also),
-      root_path
-    ].reject(&:blank?).first
+    @_meta_see_also ||= begin
+      [
+        content_for(:meta_see_also),
+        root_path
+      ].reject(&:blank?).first
+    end
   end
 
   def meta_type
-    [
-      content_for(:meta_type),
-      default_meta_type
-    ].reject(&:blank?).first
+    @_meta_type ||= begin
+      [
+        content_for(:meta_type),
+        default_meta_type
+      ].reject(&:blank?).first
+    end
   end
 
   def meta_url
-    [
-      content_for(:meta_url),
-      (request.base_url + request.path)
-    ].reject(&:blank?).first
+    @_meta_url ||= begin
+      [
+        content_for(:meta_url),
+        (request.base_url + request.path)
+      ].reject(&:blank?).first
+    end
   end
 
   def meta_viewport
-    [
-      content_for(:meta_viewport),
-      'width=device-width, initial-scale=1.0'
-    ].reject(&:blank?).first
+    @_meta_viewport ||= begin
+      [
+        content_for(:meta_viewport),
+        'width=device-width, initial-scale=1.0'
+      ].reject(&:blank?).first
+    end
   end
 
   def additional_meta_tags
@@ -116,22 +199,26 @@ module MetaHelper
     end
 
     def build_page_description_from_record
-      description = build_page_title_from_record.try(:to_page_description) ||
-        build_page_title_from_record.try(:description)
-      truncate(description, length: 240)
+      @_build_page_description_from_record ||= begin
+        description = build_page_title_from_record.try(:to_page_description) ||
+          build_page_title_from_record.try(:description)
+        truncate(description, length: 240)
+      end
     end
 
     def build_page_featured_image_from_record
-      image = record_to_build_from.try(:featured_image)
-      if image.present?
-        url = image.try(:attachment).try(:url, :large)
+      @_build_page_featured_image_from_record ||= begin
+        record_to_build_from.try(:featured_image).presence ||
+          record_to_build_from.try(:media_item).presence
       end
-      url
     end
 
     def record_to_build_from(record = nil)
       @_record_to_build_from ||= begin
-        record.presence || @page.presence || @record.presence
+        record.presence ||
+          @page.presence ||
+          @record.presence ||
+          instance_variable_get("@#{controller_name.singularize}").presence
       end
     end
 end
