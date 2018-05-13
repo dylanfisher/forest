5282f89b0c1dcfaf45587e1a2d3dc7f674260096
dylanfisher <hi@dylanfisher.com>
2018-05-12 23:57:16 -0400

    Update render_blocks to return early if a record doesn't have any blocks

--- app/helpers/block_helper.rb
+++ app/helpers/block_helper.rb
@@ -1,6 +1,8 @@
 module BlockHelper
 
   def render_blocks(record, options = {})
+    return if record.blank?
+
     begin
       block_layout = options[:block_layout].presence || BlockLayout.default_layout
       if block_layout.is_a?(Symbol)
--- lib/forest/version.rb
+++ lib/forest/version.rb
@@ -1,3 +1,3 @@
 module Forest
-  VERSION = '0.37.1'
+  VERSION = '0.37.2'
 end
