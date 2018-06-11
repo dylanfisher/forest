89bc0604f9b9853014e3b55c2e86e35f5e1fb248
dylanfisher <hi@dylanfisher.com>
2018-06-11 13:56:51 -0400

    Update AWS rake tasks to look AWS keys stored in credentials

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -1,12 +1,17 @@
-if ENV['AWS_ACCESS_KEY_ID'].present? &&
-   ENV['AWS_SECRET_KEY_ID'].present? &&
-   ENV['S3_BUCKET_NAME'].present? &&
-   ENV['AWS_REGION'].present?
+aws_access_key_id = ENV['AWS_ACCESS_KEY_ID'].presence || Rails.application.credentials&.dig(:aws, :aws_access_key_id)
+aws_secret_key_id = ENV['AWS_SECRET_KEY_ID'].presence || Rails.application.credentials&.dig(:aws, :aws_secret_key_id)
+s3_bucket_name = ENV['S3_BUCKET_NAME'].presence || Rails.application.credentials&.dig(:aws, :s3_bucket_name)
+aws_region = ENV['AWS_REGION'].presence || Rails.application.credentials&.dig(:aws, :aws_region)
+
+if aws_access_key_id.present? &&
+   aws_secret_key_id.present? &&
+   s3_bucket_name.present? &&
+   aws_region.present?
 
   Aws.config.update({
-    region: ENV['AWS_REGION'],
-    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_KEY_ID']),
+    region: aws_region,
+    credentials: Aws::Credentials.new(aws_access_key_id, aws_secret_key_id),
   })
 
-  S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['S3_BUCKET_NAME'])
+  S3_BUCKET = Aws::S3::Resource.new.bucket(s3_bucket_name)
 end
