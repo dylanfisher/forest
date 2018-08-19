97a270aef238e9edb5ee7e038b2a3c3fd1c3457c
dylanfisher <hi@dylanfisher.com>
2018-08-19 10:36:45 -0400

    Automatically add stored_attributes to whitelist page params

--- app/controllers/admin/pages_controller.rb
+++ app/controllers/admin/pages_controller.rb
@@ -84,7 +84,9 @@ class Admin::PagesController < Admin::ForestController
       # TODO: come up with a better pattern for adding additional params via the host app, rather than
       # just permitting all other column_names of the class.
       column_names = (@page.class.column_names.collect(&:to_sym) - page_attributes).reject { |a| [:id, :created_at, :updated_at].include?(a) }
-      permitted_attributes = page_attributes + column_names
+      # Attributes from store_accessors
+      stored_attributes = @page.class.stored_attributes.values.flatten
+      permitted_attributes = page_attributes + column_names + stored_attributes
       params.require(:page).permit(permitted_attributes)
     end
 
