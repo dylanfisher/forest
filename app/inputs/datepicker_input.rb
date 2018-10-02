697df451871eae799a5032d3b3c74dfd8efa093a
dylanfisher <hi@dylanfisher.com>
2018-10-02 11:46:11 -0400

    Add placholder text to date inputs

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -1,7 +1,10 @@
 class DatepickerInput < SimpleForm::Inputs::StringInput
   def input(wrapper_options = nil)
+    placeholder = options.delete(:placeholder) || 'Click to choose a date'
+
     input_html_options[:data] ||= {}
     input_html_options[:data][:datepicker] = true
+    input_html_options[:placeholder] = placeholder
 
     super
   end
