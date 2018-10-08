9d9f403d9b6966ddfb07139489c80ab67a06281a
dylanfisher <hi@dylanfisher.com>
2018-10-07 21:04:36 -0400

    Set datepicker input autocomplete to false and add cursor pointer ot input

--- app/inputs/datepicker_input.rb
+++ app/inputs/datepicker_input.rb
@@ -5,6 +5,7 @@ class DatepickerInput < SimpleForm::Inputs::StringInput
     input_html_options[:data] ||= {}
     input_html_options[:data][:datepicker] = true
     input_html_options[:placeholder] = placeholder
+    input_html_options[:autocomplete] = 'off'
 
     super
   end
