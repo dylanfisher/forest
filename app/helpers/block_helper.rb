faabb992f1772b9e29ed62eaad473d3980f265f1
dylanfisher <hi@dylanfisher.com>
2018-11-06 15:53:44 -0500

    Update render_blocks helper to only render wrapper div when present?

--- app/helpers/block_helper.rb
+++ app/helpers/block_helper.rb
@@ -5,12 +5,17 @@ module BlockHelper
 
     begin
       block_layout = options[:block_layout].presence || BlockLayout.default_layout
+
       if block_layout.is_a?(Symbol)
         block_layout = BlockLayout.find_by_slug(block_layout)
       end
 
-      content_tag :div, class: "blocks block-layout--#{block_layout.slug}" do
-        render partial: 'blocks/show', collection: record.blocks(block_layout: block_layout).reject(&:hidden?), as: 'block'
+      collection = record.blocks(block_layout: block_layout).reject(&:hidden?)
+
+      if collection.present?
+        content_tag :div, class: "blocks block-layout--#{block_layout.slug}" do
+          render partial: 'blocks/show', collection: collection, as: 'block'
+        end
       end
     rescue Exception => e
       forest_admin_error(e)
