41437ba22d5996c98388a69d269a99c8845a40c3
dylanfisher <hi@dylanfisher.com>
2019-05-08 10:16:24 -0400

    Move BlockLayout default_layout scope to class method
    Rails 6.1 adds a deprecation notice related to class level scopes in
    this instance.

--- app/models/block_layout.rb
+++ app/models/block_layout.rb
@@ -5,10 +5,12 @@ class BlockLayout < Forest::ApplicationRecord
 
   validates_presence_of :display_name
 
-  scope :default_layout, -> { BlockLayout.find_by_slug('default') }
-
   def self.resource_description
     "A block layout is a grouping of blocks that may appear in a particular part of the page. For example, you may have block layouts that apply to the sidebar or header areas of a page."
+  end
+
+  def self.default_layout
+    BlockLayout.find_by_slug('default')
   end
 
   def slug_attribute
