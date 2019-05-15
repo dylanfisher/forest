761434463c29c9c0728cd410d0ddee2fbbb8c9b2
dylanfisher <hi@dylanfisher.com>
2019-05-15 13:47:18 -0400

    Automatically infer glyphicon from block class name

--- lib/generators/forest/block/templates/model.rb
+++ lib/generators/forest/block/templates/model.rb
@@ -12,8 +12,30 @@ class <%= class_name %> < BaseBlock
     '<%= class_name.titleize %>'
   end
 
+<% if class_name.match(/image/i) -%>
+  def self.display_icon
+    'glyphicon glyphicon-picture'
+  end
+<% elsif class_name.match(/video/i) -%>
+  def self.display_icon
+    'glyphicon glyphicon-play'
+  end
+<% elsif class_name.match(/title/i) -%>
+  def self.display_icon
+    'glyphicon glyphicon-font'
+  end
+<% elsif class_name.match(/text/i) -%>
+  def self.display_icon
+    'glyphicon glyphicon-align-left'
+  end
+<% elsif class_name.match(/accordion/i) -%>
+  def self.display_icon
+    'glyphicon glyphicon-th-list'
+  end
+<% else -%>
   # def self.display_icon
   #   'glyphicon glyphicon-align-left'
   # end
+<% end -%>
 end
 <% end -%>
