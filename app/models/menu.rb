26f68b270074f260fae59f644e4462231400d54a
dylanfisher <hi@dylanfisher.com>
2019-06-17 16:26:53 -0400

    Add automatic localization support to menus via I18n.available_locales

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -53,8 +53,28 @@ class Menu < Forest::ApplicationRecord
       self.class.expire_cache!
     end
 
+    def relative_link?(resource)
+      URI(resource).relative?
+    end
+
     def create_nokogiri_children(menu_item, node)
-      node.add_child("<li><a href='#{menu_item['url']}'>#{menu_item['name']}</a></li>")
+      if menu_item['url'].first == '/' && relative_link?(menu_item['url']) && I18n.available_locales.length > 1
+        menu_item_link = "/#{I18n.locale}#{menu_item['url']}"
+      else
+        menu_item_link = menu_item['url']
+      end
+
+      if I18n.available_locales.length > 1
+        name_attr_suffix = I18n.locale == I18n.default_locale ? '' : "_#{I18n.locale}"
+        menu_item_name = menu_item["name#{name_attr_suffix}"]
+        if menu_item_name.blank?
+          menu_item_name = menu_item['name']
+        end
+      else
+        menu_item_name = menu_item['name']
+      end
+
+      node.add_child("<li><a href='#{menu_item_link}'>#{menu_item_name}</a></li>")
       if menu_item['children'].present?
         node.css('> li').last.add_child('<ul></ul>')
         menu_item['children'].each do |menu_item_child|
