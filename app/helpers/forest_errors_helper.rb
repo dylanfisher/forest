b27b06fbae234bb80e58bf198fdd44f1d5ca4ae7
dylanfisher <hi@dylanfisher.com>
2019-09-03 11:40:00 -0400

    Remove developer email from forest error helper

--- app/helpers/forest_errors_helper.rb
+++ app/helpers/forest_errors_helper.rb
@@ -15,11 +15,6 @@ module ForestErrorsHelper
 
         visible_errors_for_admin = []
         visible_errors_for_admin << options.fetch(:message, "Warning: this code block has errors and is not visible to the public.\nYou are seeing this message because you are an admin.\n")
-        visible_errors_for_admin << mail_to('mailto:hi@dylanfisher.com',
-                                        "Click here to email this error to the developers.\n",
-                                        subject: "Error on #{Rails.application.class.parent.to_s.titleize} - #{request.path.force_encoding('utf-8')}",
-                                        body: error_messages.join("\n").html_safe,
-                                        target: '_blank')
         visible_errors_for_admin << "#{error.message}\n"
         visible_errors_for_admin << error.backtrace.join("\n")
 
