09c22dca6c0a69213c9df4973d898fbee9085f5c
dylanfisher <hi@dylanfisher.com>
2020-01-13 16:14:59 -0500

    Fix error calling force_encoding on request base_url in latest Rack

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -165,7 +165,7 @@ module MetaHelper
     @_meta_url ||= begin
       [
         content_for(:meta_url),
-        (request.base_url.force_encoding('utf-8') + request.path.force_encoding('utf-8'))
+        (request.base_url.dup.force_encoding('utf-8') + request.path.force_encoding('utf-8'))
       ].reject(&:blank?).first
     end
   end
