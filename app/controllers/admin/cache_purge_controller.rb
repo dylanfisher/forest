841784a46bac1732d1783d6afb29b43d564bf21a
dylanfisher <hi@dylanfisher.com>
2020-07-30 00:14:05 -0400

    Revert "Expire application cache key when purging cache from admin screen"
    This reverts commit 1a7ac0c28d3bf1fad0206f3064cf43c585ae4a1c.

--- app/controllers/admin/cache_purge_controller.rb
+++ app/controllers/admin/cache_purge_controller.rb
@@ -5,7 +5,6 @@ class Admin::CachePurgeController < Admin::ForestController
     logger.info "[Forest] Performing user-issued cache clear"
 
     Rails.cache.clear
-    Setting.expire_application_cache_key!
 
     redirect_to params.delete(:return_to) || admin_path, notice: "Cache has been cleared."
   end
