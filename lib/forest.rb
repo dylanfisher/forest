56d4f42a0aa7ec671e0d0fd6cf69f415c4102274
dylanfisher <hi@dylanfisher.com>
2021-06-24 17:22:05 -0400

    Refactor image derivative options for easier customization in host app

--- lib/forest.rb
+++ lib/forest.rb
@@ -7,21 +7,57 @@ require 'simple_form'
 module Forest
   ROOT_PATH = Pathname.new(File.join(__dir__, ".."))
 
-  mattr_accessor :image_derivative_thumb_size
-  @@image_derivative_thumb_size = 200
+  # Each of the thumb, small, medium and large derivatives are used by Forest CMS in the
+  # admin area, but these options can be changed, and additional derivatives can be
+  # added via the host app if necessary.
+  mattr_accessor :image_derivative_thumb_options
+  @@image_derivative_thumb_options = {
+    resize: {
+      width: 200, height: 200, fit: 'cover', withoutEnlargement: true
+    },
+    jpeg: {
+      quality: 60, trellisQuantisation: true, overshootDeringing: true, optimiseScans: true, progressive: true, quantisationTable: 3
+    },
+    sharpen: true
+  }
 
-  mattr_accessor :image_derivative_small_size
-  @@image_derivative_small_size = 600
+  mattr_accessor :image_derivative_small_options
+  @@image_derivative_small_options = {
+    resize: {
+      width: 600, height: 600, fit: 'inside', withoutEnlargement: true
+    },
+    jpeg: {
+      quality: 60, trellisQuantisation: true, overshootDeringing: true, optimiseScans: true, progressive: true, quantisationTable: 3, chromaSubsampling: '4:4:4'
+    },
+    sharpen: true
+  }
 
-  mattr_accessor :image_derivative_medium_size
-  @@image_derivative_medium_size = 1200
+  mattr_accessor :image_derivative_medium_options
+  @@image_derivative_medium_options = {
+    resize: {
+      width: 1200, height: 1200, fit: 'inside', withoutEnlargement: true
+    },
+    jpeg: {
+      quality: 70, trellisQuantisation: true, overshootDeringing: true, optimiseScans: true, progressive: true, quantisationTable: 3, chromaSubsampling: '4:4:4'
+    },
+    sharpen: true
+  }
 
-  mattr_accessor :image_derivative_large_size
-  @@image_derivative_large_size = 2200
+  mattr_accessor :image_derivative_large_options
+  @@image_derivative_large_options = {
+    resize: {
+      width: 2200, height: 1600, fit: 'inside', withoutEnlargement: true
+    },
+    jpeg: {
+      quality: 70, trellisQuantisation: true, overshootDeringing: true, optimiseScans: true, progressive: true, quantisationTable: 3, chromaSubsampling: '4:4:4'
+    }
+  }
 
   # Configure forest's default values in an initializer in your host app.
   # Forest.setup do |config|
-  #   config.image_derivative_large_size = 3000
+  #   config.image_derivative_large_options[:resize][:width] = 3000
+  #   config.image_derivative_large_options[:resize][:height] = 3000
+  #   config.image_derivative_large_options[:jpeg][:quality] = 90
   # end
   def self.setup
     yield self
