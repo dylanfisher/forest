5508c2c193a9857d640dd10a1d4dc4ed34321712
dylanfisher <hi@dylanfisher.com>
2021-06-25 22:24:24 -0400

    Add Shrine add_metadata plugin and fix media item reprocessing form

--- lib/forest/shrine.rb
+++ lib/forest/shrine.rb
@@ -12,13 +12,14 @@ if s3_options
 end
 
 Shrine.plugin :activerecord           # Load Active Record integration
+Shrine.plugin :add_metadata
 Shrine.plugin :cached_attachment_data # For retaining cached file on form redisplays
 Shrine.plugin :determine_mime_type
 Shrine.plugin :infer_extension
 Shrine.plugin :instrumentation if (Rails.env.development? || Rails.application.config.log_level == :debug)
 Shrine.plugin :pretty_location
-Shrine.plugin :remote_url, max_size: 40*1024*1024 # ~40mb
 Shrine.plugin :refresh_metadata
+Shrine.plugin :remote_url, max_size: 40*1024*1024 # ~40mb
 Shrine.plugin :restore_cached_data    # Refresh metadata for cached files
 Shrine.plugin :type_predicates
 Shrine.plugin :uppy_s3_multipart      # Enable S3 multipart upload for Uppy https://github.com/janko/uppy-s3_multipart
