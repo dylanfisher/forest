0d7c8037f60f2661ce99b51a1d39ef552fbc4f66
dylanfisher <hi@dylanfisher.com>
2021-08-17 00:32:09 -0400

    Update has_many_ordered to remove non-unique ids

--- lib/forest/rails/active_record/has_many_ordered.rb
+++ lib/forest/rails/active_record/has_many_ordered.rb
@@ -16,7 +16,7 @@ module Forest
         accepts_nested_attributes_for through, allow_destroy: true
 
         define_method("#{singular}_ids=") do |ids|
-          ids = ids.reject(&:blank?).collect(&:to_i)
+          ids = ids.reject(&:blank?).collect(&:to_i).uniq
           super(ids)
           send(through).each do |join_model|
             join_model.send("#{order_by}=", ids.index(join_model.send(singular_id)))
