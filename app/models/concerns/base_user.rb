f8bd9d529e14bec520d9f1b1f1dcccc4b1c2e67c
dylanfisher <hi@dylanfisher.com>
2021-10-01 10:39:36 -0400

    Try to fix alignment of select2 badges

--- app/models/concerns/base_user.rb
+++ app/models/concerns/base_user.rb
@@ -46,7 +46,7 @@ module BaseUser
       img_tag = "<img src='#{media_item.attachment_url(:thumb)}' style='height: 21px; margin-right: 5px;'> "
     end
     user_group_badges = user_groups.collect { |ug| "<span class='badge badge-secondary h-100 ml-auto'>#{ug.name}</span>" }.reject(&:blank?)
-    "<div class='d-flex align-items-baseline w-100'>#{img_tag}<span class='select2-response__id mr-2' data-id='#{id}' style='margin-right: 5px;'>#{id}</span> #{to_label} #{user_group_badges.join()}</div>"
+    "<div class='d-flex align-items-baseline w-100'>#{img_tag}<span class='select2-response__id mr-2' data-id='#{id}' style='margin-right: 5px;'>#{id}</span> <span class='mx-1'>#{to_label}</span> #{user_group_badges.join()}</div>"
   end
 
   private
