dbc329aa36d8084d3ea3151821ab7392e413039e
dylanfisher <hi@dylanfisher.com>
2021-10-05 22:53:45 -0400

    Add nested_attributes_options to has_many_ordered

--- lib/forest/rails/active_record/has_many_ordered.rb
+++ lib/forest/rails/active_record/has_many_ordered.rb
@@ -7,13 +7,14 @@ module Forest
         through = options.delete(:through)
         through_options = options.delete(:through_options) || {}
         has_many_options = options.delete(:has_many_options) || {}
+        nested_attributes_options = options.delete(:nested_attributes_options) || {}
         order_by = options.delete(:order_by) || :position
         singular = association.to_s.singularize
         singular_id = (has_many_options.fetch(:source, nil).try(:to_s).try(:singularize) || singular).to_s + '_id'
 
         has_many through, -> { reorder(order_by) }, **options.reverse_merge(dependent: :destroy, **through_options)
         has_many association, -> { reorder("#{through}.#{order_by}") }, **options.merge(through: through, **has_many_options)
-        accepts_nested_attributes_for through, allow_destroy: true
+        accepts_nested_attributes_for through, **options.reverse_merge(allow_destroy: true, **nested_attributes_options)
 
         define_method("#{singular}_ids=") do |ids|
           ids = ids.reject(&:blank?).collect(&:to_i).uniq
