088c4f7609cb4faf221db4ff6f23cc7e4e67d99c
dylanfisher <hi@dylanfisher.com>
2022-01-11 18:52:52 -0500

    Change Shrine preferences to use private uploads per S3 recommendations

--- lib/forest/shrine.rb
+++ lib/forest/shrine.rb
@@ -6,8 +6,8 @@ s3_options = Rails.application.credentials.s3
 
 if s3_options
   Shrine.storages = {
-    cache: Shrine::Storage::S3.new(prefix: 'cache', public: true, **s3_options),
-    store: Shrine::Storage::S3.new(prefix: 'media', public: true, **s3_options),
+    cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
+    store: Shrine::Storage::S3.new(prefix: 'media', **s3_options),
   }
 end
 
@@ -28,4 +28,4 @@ Shrine.plugin :remote_url, max_size: 40*1024*1024 # ~40mb
 Shrine.plugin :restore_cached_data    # Refresh metadata for cached files
 Shrine.plugin :type_predicates
 Shrine.plugin :uppy_s3_multipart      # Enable S3 multipart upload for Uppy https://github.com/janko/uppy-s3_multipart
-Shrine.plugin :url_options, store: { host: Rails.application.credentials.asset_host } if Rails.application.credentials.asset_host.present?
+Shrine.plugin :url_options, store: { host: Rails.application.credentials.asset_host, public: true } if Rails.application.credentials.asset_host.present?
