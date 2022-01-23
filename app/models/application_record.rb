3c54a71863cd34ba39607861e4f31102af230c66
dylanfisher <hi@dylanfisher.com>
2022-01-23 12:22:14 -0500

    Cache dashboard panel resources

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  # Add the following code to your app's ApplicationRecord to update the application
-  # wide cache key.
+  # Add the following code to your app's ApplicationRecord to update the application wide cache key.
   after_commit :expire_application_cache_key
 
   private
