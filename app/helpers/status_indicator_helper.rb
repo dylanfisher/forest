20a4be0a4267e813015351fd59498e44371cf6bb
dylanfisher <hi@dylanfisher.com>
2022-05-04 15:26:04 -0400

    Add fallback status indicator message

--- app/helpers/status_indicator_helper.rb
+++ app/helpers/status_indicator_helper.rb
@@ -1,7 +1,8 @@
 module StatusIndicatorHelper
   def status_indicator(record)
+    default_record_status = t(record.status, scope: [:forest, :admin, :page, :status])
     content_tag(:span, '',
       class: "status-indicator status-indicator--#{record.status}",
-      title: "Page status: #{t(record.status, scope: [:forest, :admin, record.model_name.singular, :status])}")
+      title: "Record status: #{t(record.status, scope: [:forest, :admin, record.model_name.singular, :status], default: default_record_status)}")
   end
 end
