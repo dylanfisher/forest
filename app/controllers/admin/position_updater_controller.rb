074e62572fe9d63e3c5e82f34d609f934d4d10c4
dylanfisher <hi@dylanfisher.com>
2022-11-16 13:20:31 -0500

    Improve performance by avoiding update if table position didn't change

--- app/controllers/admin/position_updater_controller.rb
+++ app/controllers/admin/position_updater_controller.rb
@@ -12,10 +12,18 @@ class Admin::PositionUpdaterController < Admin::ForestController
     records_cache = resource.find(record_ids).to_a
     new_record_ids_with_position = []
 
+    position_has_changed = false
     record_ids_with_position.each do |record_id, position|
       record = records_cache.find { |r| r.id == record_id }
-      record.update(position: position)
+      position_has_changed = true if record.position != position
       new_record_ids_with_position << { id: record.id, position: position }
+    end
+
+    if position_has_changed
+      record_ids_with_position.each do |record_id, position|
+        record = records_cache.find { |r| r.id == record_id }
+        record.update(position: position)
+      end
     end
 
     render json: new_record_ids_with_position
