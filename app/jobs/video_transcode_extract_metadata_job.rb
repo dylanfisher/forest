a555298ad7b6cdeffcf1fe1fd82f24ee9ea88389
dylanfisher <hi@dylanfisher.com>
2023-07-26 20:40:44 -0700

    Update VideoTranscodeExtractMetadataJob to set proper cache control

--- app/jobs/video_transcode_extract_metadata_job.rb
+++ app/jobs/video_transcode_extract_metadata_job.rb
@@ -1,7 +1,37 @@
 class VideoTranscodeExtractMetadataJob < ApplicationJob
   LAMBDA_FUNCTION_NAME = 'ffprobe'
+  # https://mattboldt.com/updating-s3-object-metadata-in-ruby/
+  COPY_TO_OPTIONS = [:multipart_copy, :content_length, :copy_source_client, :copy_source_region, :acl, :cache_control, :content_disposition,
+    :content_encoding, :content_language, :content_type, :copy_source_if_match, :copy_source_if_modified_since, :copy_source_if_none_match,
+    :copy_source_if_unmodified_since, :expires, :grant_full_control, :grant_read, :grant_read_acp, :grant_write_acp, :metadata, :metadata_directive,
+    :tagging_directive, :server_side_encryption, :storage_class, :website_redirect_location, :sse_customer_algorithm, :sse_customer_key,
+    :sse_customer_key_md5, :ssekms_key_id, :copy_source_sse_customer_algorithm, :copy_source_sse_customer_key, :copy_source_sse_customer_key_md5,
+    :request_payer, :tagging, :use_accelerate_endpoint]
 
   def perform(media_item_id:, object_path:)
+    media_item = MediaItem.find(media_item_id)
+
+    # Add proper cache control metadata to the object
+    filename_from_object_path = object_path.split('/').last
+    s3_object = s3_bucket.object(object_path)
+    options = {}
+    existing_options = s3_object.data.to_h.slice(*COPY_TO_OPTIONS)
+    options.merge!(existing_options)
+    options.merge!({
+      cache_control: 'public, max-age=31536000',
+      metadata_directive: 'REPLACE'
+    })
+
+    # multipart_copy is necessary if the object is 5GB+
+    if s3_object.size >= 5_000_000_000
+      options.merge!({multipart_copy: true})
+    else
+      # Only used if multipart_copy is true
+      options.delete(:content_length)
+    end
+
+    s3_object.copy_to("#{Forest.config[:aws_bucket]}/#{object_path}", options)
+
     # https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/Lambda/Client.html#invoke-instance_method
     response = lambda_client.invoke({
       function_name: LAMBDA_FUNCTION_NAME,
@@ -22,7 +52,7 @@ class VideoTranscodeExtractMetadataJob < ApplicationJob
     response_json = JSON.parse(response.payload.string)
     response_body = response_json['body']
 
-    media_item = MediaItem.find(media_item_id)
+    media_item.reload
 
     # Update media item immediately to avoid race conditions when multiple jobs are called on the same media item
     if media_item.video_data.class != Hash
@@ -47,5 +77,9 @@ class VideoTranscodeExtractMetadataJob < ApplicationJob
       region: Forest.config[:aws_region],
       credentials: Aws::Credentials.new(Aws.config[:credentials].access_key_id, Aws.config[:credentials].secret_access_key)
     })
+  end
+
+  def s3_bucket
+    @s3_bucket ||= Aws::S3::Bucket.new(Forest.config[:aws_bucket])
   end
 end
