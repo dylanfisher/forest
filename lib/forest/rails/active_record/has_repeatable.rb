af74f81cea061e04c9d3eab853cc1f016b39060f
dylanfisher <hi@dylanfisher.com>
2023-11-18 19:56:09 -0800

    Fix deprecated key warning and unused (I think) serialize coder
    I don't think Array was a valid Coder, and I _think_ it was defaulting
    to YAML anyway. If that's wrong this will break something somewhere.

--- lib/forest/rails/active_record/has_repeatable.rb
+++ lib/forest/rails/active_record/has_repeatable.rb
@@ -6,7 +6,7 @@ module Forest
 
     class_methods do
       def has_repeatable(attribute, options = {})
-        serialize attribute, Array
+        serialize attribute
 
         before_save :"remove_blank_#{attribute}"
 
