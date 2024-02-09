10db690f465ca67f7e4f37e5e2ea30a88396b080
dylanfisher <hi@dylanfisher.com>
2024-02-09 10:57:13 -0800

    Fix error in cache helper try method

--- app/helpers/cache_helper.rb
+++ app/helpers/cache_helper.rb
@@ -3,12 +3,12 @@ module CacheHelper
     count = collection.try(:count) || 1
     if collection.is_a? Array
       return if collection.blank?
-      max_updated_at = collection.reject(&:blank?).max_by(&:updated_at).updated_at.try(:utc).try(:to_s, :number)
+      max_updated_at = collection.reject(&:blank?).max_by(&:updated_at).updated_at.try(:to_i)
       "#{collection.first.model_name.plural}/all-#{count}-#{max_updated_at}"
     elsif collection.try :updated_at
-      "#{collection.model_name.plural}/all-#{count}-#{collection.updated_at.try(:utc).try(:to_s, :number)}"
+      "#{collection.model_name.plural}/all-#{count}-#{collection.updated_at.try(:to_i)}"
     else
-      max_updated_at = collection.maximum(:updated_at).try(:utc).try(:to_s, :number)
+      max_updated_at = collection.maximum(:updated_at).try(:to_i)
       "#{collection.model_name.plural}/all-#{count}-#{max_updated_at}"
     end
   end
